2d6fb3b34e4d85bba212614a2d5e52a713a9625f
Larry Lade <lplade@users.noreply.github.com>
2017-03-14 16:29:27 -0500

    Updated model

--- wishlist/travel_wishlist/models.py
+++ wishlist/travel_wishlist/models.py
@@ -1,4 +1,6 @@
+from datetime import date
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -6,6 +8,8 @@ from django.db import models
 class Place(models.Model):
     name = models.CharField(max_length=200)
     visited = models.BooleanField(default=False)
+    date_visited = models.DateField(default=timezone.now)
+    notes = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return '%s visited? %s' % (self.name, self.visited)
